d5facc62d1ecaa16eabc16cbaab2fd2cee2d110c
Dwight Van der Velpen <Dfr34kz@gmail.com>
2018-04-16 20:07:43 +0200

    Yeah better create a checkpoint. Can have some cubes etc. Pretty wild stuff

---
+++ Labo 01 - Simple maze/Gui/MazeGame/Block.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+
+namespace MazeGame
+{
+    public class Block
+    {
+        // Helpers
+        private static Int32[] INDICES = { 0, 1, 2, 0, 2, 3, 4, 7, 6, 4, 6, 5, 4, 0, 3, 4, 3, 7, 1, 5, 6, 1, 6, 2, 9, 8, 12, 9, 12, 13, 10, 14, 15, 10, 15, 11 };
+        
+        // Fields
+        public GeometryModel3D Model = new GeometryModel3D(); 
+        public MeshGeometry3D Mesh = new MeshGeometry3D();
+        public Point3DCollection Corners = new Point3DCollection();
+
+        // Constructors
+        public Block(Point3D[] points, string textureUrl)
+        {
+            // Preparation
+            Int32Collection triangles = new Int32Collection();
+            PointCollection texturePoints = new PointCollection();
+
+            // Linking
+            Mesh.Positions = Corners;
+            Mesh.TriangleIndices = triangles;
+            Mesh.TextureCoordinates = texturePoints;
+            Model.Geometry = Mesh;
+            Model.Material = new DiffuseMaterial(new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri(textureUrl, UriKind.Relative))
+            });
+
+            // Corners, twice (TextureFix)
+            for(int i = 0; i < 2; i++) foreach (var point in points) Corners.Add(point);
+
+            // Indices, grouped per 3 decide wich corners make a triangle            
+            foreach (var indice in INDICES) triangles.Add(indice);
+
+            // Textures, Same as corners
+            texturePoints.Add(new Point(1, 0));
+            texturePoints.Add(new Point(0, 0));
+            texturePoints.Add(new Point(0, 1));
+            texturePoints.Add(new Point(1, 1));
+
+            texturePoints.Add(new Point(0, 0));
+            texturePoints.Add(new Point(1, 0));
+            texturePoints.Add(new Point(1, 1));
+            texturePoints.Add(new Point(0, 1));
+
+            texturePoints.Add(new Point(1, 1));
+            texturePoints.Add(new Point(0, 1));
+            texturePoints.Add(new Point(0, 0));
+            texturePoints.Add(new Point(1, 0));
+
+            texturePoints.Add(new Point(1, 0));
+            texturePoints.Add(new Point(0, 0));
+            texturePoints.Add(new Point(0, 1));
+            texturePoints.Add(new Point(1, 1));
+        }
+    }
+}
---
+++ Labo 01 - Simple maze/Gui/MazeGame/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MazeGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Simple block
+            Point3D[] blokPunten = { new Point3D(0.5, 0.5, 0.5), new Point3D(-0.5, 0.5, 0.5), new Point3D(-0.5, -0.5, 0.5), new Point3D(0.5, -0.5, 0.5), new Point3D(0.5, 0.5, -0.5), new Point3D(-0.5, 0.5, -0.5), new Point3D(-0.5, -0.5, -0.5), new Point3D(0.5, -0.5, -0.5)};
+            var blok = new Block(blokPunten, @"Resources\Hout.jpg");
+            var blok2 = new Block(blokPunten, @"Resources\Hout.jpg");
+            // Simple light
+            var light = new AmbientLight()
+            {
+                Color = Colors.White
+            };
+
+            // Simple camera
+            var camera = new PerspectiveCamera()
+            {
+                FieldOfView = 45,
+                Position = new Point3D(0,2,3),
+                LookDirection = new Vector3D(0,-2,-3),
+                UpDirection = new Vector3D(0,1,0)
+            };
+
+            // Model elements
+            var modelGrp = new Model3DGroup();
+            modelGrp.Children.Add(blok.Model);
+            modelGrp.Children.Add(blok2.Model);
+            modelGrp.Children.Add(light);
+
+            // Viewport
+            var viewport = new Viewport3D()
+            {
+                Camera = camera,
+                Height = 500,
+                Width = 500
+            };
+            viewport.Children.Add(new ModelVisual3D() { Content = modelGrp });
+            mainCanvas.Children.Add(viewport);
+
+            // Canvas
+            Canvas.SetTop(viewport, 0);
+            Canvas.SetLeft(viewport, 0);
+
+            // TransformGroup
+            var transforms = new Transform3DGroup();
+            blok.Model.Transform = transforms;
+            var transforms2 = new Transform3DGroup();
+            blok2.Model.Transform = transforms2;
+
+            // Rotation
+            var rotAxis = new AxisAngleRotation3D(new Vector3D(0,1,0),0);
+            var rotation = new RotateTransform3D(rotAxis);
+            var rotAngle = new DoubleAnimation()
+            {
+                From = 0,
+                To = 360,
+                Duration = new Duration(TimeSpan.FromSeconds(20.0)),
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+            transforms.Children.Add(rotation);
+            transforms2.Children.Add(rotation);
+
+            // Translate
+            transforms2.Children.Add(new TranslateTransform3D(.77, 0, 0));
+            transforms.Children.Add(new TranslateTransform3D(-.77, 0, 0));
+
+            // Final linking (?)
+            NameScope.SetNameScope(mainCanvas, new NameScope());
+            mainCanvas.RegisterName("rotAxis", rotAxis);
+            Storyboard.SetTargetName(rotAngle, "rotAxis");
+            Storyboard.SetTargetProperty(rotAngle, new PropertyPath(AxisAngleRotation3D.AngleProperty));
+            var rotCube = new Storyboard();
+            rotCube.Children.Add(rotAngle);
+            rotCube.Begin(mainCanvas);
+        }
+    }
+}
